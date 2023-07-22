2c63800720d734cf46b089d0ae3b957d0b5398c7
Kotikf <76152485+Kotikf@users.noreply.github.com>
2023-07-22 12:13:02 +0300

    Update bot.py

--- Sticker_bot/bot.py
+++ Sticker_bot/bot.py
@@ -1,4 +1,5 @@
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton
 
 
 token = ''
@@ -6,10 +7,24 @@ token = ''
 bot = Bot(token)
 dp = Dispatcher(bot)
 
+kb = ReplyKeyboardMarkup(resize_keyboard=True)
+kb.add(KeyboardButton('/help'))
+
+@dp.message_handler(commands=['start'])
+async def command_start(message: types.Message):
+    await bot.send_message(chat_id=message.from_user.id, text='Привет, отправь мне стикер и получи его id', reply_markup=kb)
 
 @dp.message_handler(content_types=['sticker'])
 async def send_sticker_id(message: types.Message):
     await message.answer(message.sticker.file_id)
+
+@dp.message_handler(commands=['help'])
+async def command_help(message: types.Message):
+    await message.answer('Просто отправь мне и стикер и ты получешь его id')
+
+@dp.message_handler()
+async def send_sticker_id(message: types.Message):
+    await message.answer('Отправляй только стикер')
 
 
 if __name__ == '__main__':
