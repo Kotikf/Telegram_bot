a48f7676d373adfa2a72d269ffd23ff2f3892b8c
Kotikf <76152485+Kotikf@users.noreply.github.com>
2023-07-22 11:47:25 +0300

    Add files via upload

--- Sticker_bot/bot.py
+++ Sticker_bot/bot.py
@@ -1,1 +1,16 @@
+from aiogram import Bot, Dispatcher, executor, types
 
+
+token = ''
+
+bot = Bot(token)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(content_types=['sticker'])
+async def send_sticker_id(message: types.Message):
+    await message.answer(message.sticker.file_id)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
